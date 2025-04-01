d6258c9dcf70ae5a0affdf56ede908ad26ff30c9
Alexis <alexis.chabauty@reseau.eseo.fr>
2025-04-01 09:15:35 +0200

    merge_v2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,8 +30,9 @@ public class MainActivity extends AppCompatActivity {
         createGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Action à effectuer lorsque le bouton "Créer une partie" est cliqué
-                Toast.makeText(MainActivity.this, "Créer une partie cliqué", Toast.LENGTH_SHORT).show();
+                // Lancer LoginActivity lorsque le bouton "Créer une partie" est cliqué
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
